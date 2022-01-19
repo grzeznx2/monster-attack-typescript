7b2a71f68ad7999e46cb3c7e688c86858ac2b31c
grzeznx2 <grzeznx2@gmail.com>
2022-01-19 19:51:29 +0100

    scaffold GameState class

---
+++ src/GameState.ts
@@ -0,0 +1,29 @@
+import { Defender } from './Defender'
+import { Enemy } from './Enemy'
+import { FloatingMessage } from './FloatingMessage'
+import { Resource } from './Resource'
+
+export enum GameStatus {
+  Pending = 'PENDING',
+  Lost = 'LOST',
+  Won = 'WON',
+}
+
+export class GameState {
+  defenders: Defender[] = []
+  enemies: Enemy[] = []
+  enemiesInterval: number
+  enemiesPositions: number[] = []
+  floatingMessages: FloatingMessage[] = []
+  resources: Resource[] = []
+  resourcesCount: number = 0
+  score: number = 0
+  scoreToWin: number
+  frame: number = 0
+  status: GameStatus = GameStatus.Pending
+
+  constructor({ scoreToWin, enemiesInterval }: { scoreToWin: number; enemiesInterval: number }) {
+    this.scoreToWin = scoreToWin
+    this.enemiesInterval = enemiesInterval
+  }
+}
