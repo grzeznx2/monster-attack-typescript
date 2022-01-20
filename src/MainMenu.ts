101bdb04b112d385dc5b62b170631f0a60a7291a
grzeznx2 <grzeznx2@gmail.com>
2022-01-20 18:53:16 +0100

    refactor && set game status info

--- src/MainMenu.ts
+++ src/MainMenu.ts
@@ -4,28 +4,38 @@ import { game } from './Game'
 const MAIN_MENU_ID = 'main-menu'
 const NEW_GAME_BUTTON_ID = 'new-game-button'
 const SETTINGS_BUTTON_ID = 'new-game-button'
+const GAME_STATUS_ID = 'game-status'
 
 export class MainMenu extends Common<HTMLDivElement> {
   newGameButton: HTMLButtonElement
   settingsButton: HTMLButtonElement
+  gameStatus: HTMLParagraphElement
   constructor() {
     super(MAIN_MENU_ID)
     this.newGameButton = this.bindToElement<HTMLButtonElement>(NEW_GAME_BUTTON_ID)
     this.settingsButton = this.bindToElement<HTMLButtonElement>(SETTINGS_BUTTON_ID)
+    this.gameStatus = this.bindToElement<HTMLParagraphElement>(GAME_STATUS_ID)
     this.newGameButton.addEventListener('click', () => this.play())
   }
 
-  showNextLevelMenu() {
+  setGameStatusText(value: string) {
+    this.gameStatus.innerHTML = value
+  }
+
+  handleLevelWon() {
+    this.setGameStatusText('You Won!')
     this.changeVisibilityScreen(this.element!, ScreenVisibility.Visible)
     this.newGameButton.innerHTML = 'Next Level'
   }
 
-  showGameLostMenu() {
+  handleLevelLost() {
+    this.setGameStatusText('You Lost!')
     this.changeVisibilityScreen(this.element!, ScreenVisibility.Visible)
     this.newGameButton.innerHTML = 'Try Again'
   }
 
   play() {
+    this.setGameStatusText('')
     this.changeVisibilityScreen(this.element!, ScreenVisibility.Hidden)
     game.start()
   }
