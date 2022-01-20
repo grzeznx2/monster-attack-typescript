cecc0ee528982b924b1a5faba5cd64eaaf616ef1
grzeznx2 <grzeznx2@gmail.com>
2022-01-20 18:20:28 +0100

    refactor FloatingMessage class to inherit from Entity

--- src/FloatingMessage.ts
+++ src/FloatingMessage.ts
@@ -1,22 +1,30 @@
 import { canvas } from './Canvas'
+import { Entity } from './Entity'
 
-export class FloatingMessage {
-  color: string
-  health: number = 50
-  opacity: number = 1
+export class FloatingMessage extends Entity {
+  protected _width: number
+  protected _height: number
+  protected _maxHealth: number = 50
+  protected _health = this.maxHealth
+  protected _x: number
+  protected _y: number
+  protected _maxSpeed: number = 0.3
+  protected _currentSpeed: number = this._maxSpeed
   size: number
+  color: string
+  opacity: number = 1
   value: string
-  x: number
-  y: number
 
   constructor(value: string, x: number, y: number, size: number, color: string) {
+    super()
     this.value = value
-    this.x = x
-    this.y = y
+    this._x = x
+    this._y = y
     this.size = size
+    this._width = size
+    this._height = size
     this.color = color
     this.opacity = 1
-    this.health = 50
   }
 
   draw() {
@@ -29,30 +37,9 @@ export class FloatingMessage {
     }
   }
 
-  private updateHealth(value: number) {
-    this.health += value
-  }
-
-  private updateX(value: number) {
-    this.x += value
-  }
-
-  private updateY(value: number) {
-    this.y += value
-  }
-
   update() {
-    this.move({ deltaY: -0.3 })
+    this.move({ deltaY: -this._currentSpeed })
     this.updateHealth(-1)
     if (this.opacity > 0.02) this.opacity -= 0.02
-  }
-
-  move({ deltaX, deltaY }: { deltaX?: number; deltaY?: number }) {
-    if (deltaX) this.updateX(deltaX)
-    if (deltaY) this.updateY(deltaY)
-  }
-
-  get shouldBeRemoved() {
-    return this.health <= 0
   }
 }
