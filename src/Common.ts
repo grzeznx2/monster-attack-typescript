0e5efbceb76ee5be7501d3e23b7cd5e6dc927374
grzeznx2 <grzeznx2@gmail.com>
2022-01-20 16:33:02 +0100

    toggle element's visibility

--- src/Common.ts
+++ src/Common.ts
@@ -1,14 +1,26 @@
+export const HIDDEN_CLASS = 'hidden'
+export enum ScreenVisibility {
+  Visible,
+  Hidden,
+}
+
 export abstract class Common<T extends HTMLElement> {
   public element: T | null = null
 
   constructor(id: string) {
     if (typeof id === 'undefined') return
-    this.element = this.bindToElement(id)
+    this.element = this.bindToElement(id) as T
   }
 
-  bindToElement(id: string): T {
-    const element = document.getElementById(id) as T
+  bindToElement<U extends HTMLElement = T>(id: string): U {
+    const element = document.getElementById(id) as U
     if (!element) throw new Error(`COMMON: Cannot find the element with id: ${id}`)
     return element
+  }
+
+  changeVisibilityScreen(element: HTMLElement, mode: ScreenVisibility) {
+    mode === ScreenVisibility.Visible
+      ? element.classList.remove(HIDDEN_CLASS)
+      : element.classList.add(HIDDEN_CLASS)
   }
 }
